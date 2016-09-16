0e92bacbe32a8690b51920605c822d3733af8de0
Swizec Teller <swizec@swizec.com>
2016-09-16 00:29:29 -0700

    Improved experiment runner

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import * as d3 from 'd3';
 import _ from 'lodash';
+import async from 'async';
 
 class App extends Component {
-    state = {results: []}
-    N = 10
+    state = {results: [],
+             done: false}
+    N = 20
 
     componentDidMount() {
         d3.csv('http://swizec.github.io/h1b-software-salaries/data/h1bs.csv')
@@ -20,9 +22,31 @@ class App extends Component {
     }
 
     experiment() {
-        this.runner('lodash', _.cloneDeep);
-        this.runner('json.string -> parse', (d) => JSON.parse(JSON.stringify(d)));
-        this.runner('Object.assign', (arr) => arr.map((d) => Object.assign({}, d)));
+        const experiments = {
+            'lodash _.cloneDeep': _.cloneDeep,
+            '.map + lodash _.clone': (arr) => arr.map((d) => _.clone(d)),
+            '.map + lodash _.assign': (arr) => arr.map((d) => _.assign({}, d)),
+            'JSON string/parse': (arr) => JSON.parse(JSON.stringify(arr)),
+            '.map + Object.assign': (arr) => arr.map((d) => Object.assign({}, d))
+        };
+
+        const methods = Object.keys(experiments)
+                              .map(name => {
+                                  return (callback) => {
+                                      setTimeout(() => {
+                                          this.runner(name, experiments[name]);
+                                          callback();
+                                      },
+                                                 0);
+                                  }});
+
+        async.series(methods, () => {
+            let results = this.state.results;
+            results.push({name: '--- done ---',
+                          avg: '---'});
+            this.setState({results: results,
+                           done: true});
+        });
     }
 
     runner(name, method) {
@@ -44,16 +68,31 @@ class App extends Component {
 
     render() {
         let results = null;
+        const done = !this.state.done ? `Experiment runs ${this.N} times. Be patient :)` : '';
 
         if (this.state.data && !this.state.results.length) {
-            results = <p>Fetched {this.state.data.length}-element dataset. Making copies :)</p>;
+            results = <p>Fetched {this.state.data.length}-element dataset. Making copies. {done}</p>;
         }else if (this.state.results.length) {
+
             results =
             <div>
-                <p>Average time to copy {this.state.data.length} object array. Experiment runs {this.N} times.</p>
-                <ul>
-                    {this.state.results.map((res, i) => <li key={i}><strong>{res.name}</strong>: {res.avg}ms</li>)}
-                </ul>
+                <p>Average time to clone <strong>{this.state.data.length} object</strong> array. {done}</p>
+                <br/>
+                <table className="results">
+                    <thead>
+                        <tr>
+                            <th>Method</th><th>Avg. Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.results.map((res, i) => (
+                            <tr key={i}>
+                            <td>{res.name}</td>
+                            <td>{res.avg}ms</td>
+                            </tr>
+                         ))}
+                    </tbody>
+                </table>
             </div>
         }
 
